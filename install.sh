b922d585508d4c9b9771f2e273875e4c7c9d7366
pestech28 <pestech28@yahoo.com>
2026-06-05 17:31:34 -0400

    Disable iStart

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ sudo apt install apache2 -y
 sudo ufw allow 8888/tcp
 sudo cp tinyproxy/tinyproxy.conf /etc/tinyproxy -f
 sudo cp tinyproxy/filter /etc/tinyproxy -f
-sudo cp sites/istart.conf /etc/apache2/sites-available/ -f
-sudo a2ensite istart.conf
-echo "127.0.0.1 istart.net" | sudo tee -a /etc/hosts
+#sudo cp sites/istart.conf /etc/apache2/sites-available/ -f
+#sudo a2ensite istart.conf
+#echo "127.0.0.1 istart.net" | sudo tee -a /etc/hosts
 tar -xf www/front.tar --overwrite -C /var/www/html
-tar -xf www/istart.tar --overwrite -C /var/www/html/istart
+#tar -xf www/istart.tar --overwrite -C /var/www/html/istart
 sudo systemctl reload apache2
 sudo systemctl restart tinyproxy

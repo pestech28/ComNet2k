a5e771baa18d43ffc721ba4e7fe43867645e5280
pestech28 <pestech28@yahoo.com>
2026-06-12 14:42:45 -0400

    Added nintendo.com

--- install.sh
+++ install.sh
@@ -14,5 +14,10 @@ sudo a2ensite com.conf
 echo "127.0.0.1 com.net" | sudo tee -a /etc/hosts
 sudo tar -xvzf www/com.tar.gz --overwrite -C /var/www/html/com
 sudo mkdir /var/www/html/com/pub
+sudo mkdir /var/www/html/restore/nintendo
+sudo cp sites/nintendo.conf /etc/apache2/sites-available/ -f
+sudo a2ensite nintendo.conf
+echo "127.0.0.1 nintendo.net" | sudo tee -a /etc/hosts
+sudo tar -xvzf www/restore/nintendo.tar.gz --overwrite -C /var/www/html/restore/nintendo
 sudo systemctl reload apache2
 sudo systemctl restart tinyproxy
